368ba1faa6dd6f868df894fc1bc6fb0ee708d906
ChanceCafferty <chance.r.cafferty@gmail.com>
2022-08-04 00:30:38 -0500

    cmonnnnnn

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -2,16 +2,13 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization) next("token required");
+  if (!authorization) return res.status(400).json({ message: "token required" });
 
   try {
     jwt.verify(authorization, "secret");
     next();
   } catch (err) {
-    if (err.message === "invalid signature")
       return res.status(400).json({ message: "token invalid" });
-
-    return res.status(500).send(err);
   }
 
   /*
